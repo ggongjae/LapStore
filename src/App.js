cfaea4e4e52e2dbc78e89562ba6eb30d0b2d33a1
권건율 <cjdak0907@naver.com>
2024-01-30 16:45:02 +0900

    FIX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-// App.js
-import './App.css'
 import React, { useState } from 'react'
+import Detail from './pages/Detailpage'
+import Main from './pages/Mainpage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Result from './Result'
+import Login from './pages/Loginpage'
+import Join from './pages/Joinpage'
+import MyPage from './pages/Mypage'
+
 function App() {
-    return <div></div>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/product/:productId" element={<Detail />} />
+                <Route path="/Result" element={<Result />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Join" element={<Join />} />
+                <Route path="/Mypage" element={<MyPage />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
